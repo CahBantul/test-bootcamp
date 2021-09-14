2713e9de2abad51261359c033684049b30d036e4
Cah Bantul <fardan.nozami@gmail.com>
2021-09-14 09:33:56 +0700

    program kasir oke

--- jsonManipulation.js
+++ jsonManipulation.js
@@ -1,42 +1,42 @@
 let datas = require('./data.json');
 
-// const itemsInMeetingRoom = (datas) => {
-//   const newData = datas.filter((data) => {
-//     return data.placement.name === 'Meeting Room';
-//   });
-//   const items = newData.map((item) => item.name);
-//   return console.log(items);
-// };
+const itemsInMeetingRoom = (datas) => {
+  const newData = datas.filter((data) => {
+    return data.placement.name === 'Meeting Room';
+  });
+  const items = newData.map((item) => item.name);
+  return console.log(items);
+};
 
-// itemsInMeetingRoom(datas);
+itemsInMeetingRoom(datas);
 
-// const allElectronic = (datas) => {
-//   const newData = datas.filter((data) => {
-//     return data.type === 'electronic';
-//   });
-//   const items = newData.map((item) => item.name);
-//   return console.log(items);
-// };
-// allElectronic(datas);
+const allElectronic = (datas) => {
+  const newData = datas.filter((data) => {
+    return data.type === 'electronic';
+  });
+  const items = newData.map((item) => item.name);
+  return console.log(items);
+};
+allElectronic(datas);
 
-// const allFurniture = (datas) => {
-//   const newData = datas.filter((data) => {
-//     return data.type === 'furniture';
-//   });
-//   const items = newData.map((item) => item.name);
-//   return console.log(items);
-// };
-// allFurniture(datas);
+const allFurniture = (datas) => {
+  const newData = datas.filter((data) => {
+    return data.type === 'furniture';
+  });
+  const items = newData.map((item) => item.name);
+  return console.log(items);
+};
+allFurniture(datas);
 
-// const itemWithBrownColor = (datas) => {
-//   const newData = datas.filter((data) => {
-//     return data.tags.includes('brown'); 
-//   });
+const itemWithBrownColor = (datas) => {
+  const newData = datas.filter((data) => {
+    return data.tags.includes('brown'); 
+  });
   
-//   const items = newData.map((item) => item.name);
-//   return console.log(items);
-// };
-// itemWithBrownColor(datas);
+  const items = newData.map((item) => item.name);
+  return console.log(items);
+};
+itemWithBrownColor(datas);
 const itemsPurchasedAt = (date) => {
   const newData = datas.filter((data) => {
     return data.purchased_at == date; 
--- kasir.js
+++ kasir.js
@@ -1,12 +1,35 @@
 const cassier = (restoName, cassierName, items) => {
   const date = new Date().toISOString();
-  items.map((item) => {
-    if(item.le)
-  });
+  const item = items
+    .map((item) => {
+      let bill = `${item.name}.........Rp. ${item.price}`;
+      console.log(bill.length)
+      if (bill.length > 32) {
+        return  `${item.name}...........
+        ............Rp. ${item.price}
+        `;
+      } else {
+        return bill
+      }
+    })
+    .join('\n');
   const result = `
-  ${restoName}
-  Tanggal : ${date}
-  Nama Kasir : ${cassierName}
-  ================================
-  `;
+${restoName}
+Tanggal : ${date}
+Nama Kasir : ${cassierName}
+================================
+${item}
+    `;
+  return result;
 };
+
+const restoName = 'Rumah Makan Padang';
+const cassierName = 'Sambalado';
+const items = [
+  { name: 'nasi rendang', price: 16000 },
+  { name: 'nasi ayam', price: 15000 },
+  { name: 'nasi telur', price: 13000 },
+  { name: 'nasi ayam bakar', price: 13000 },
+];
+
+console.log(cassier(restoName, cassierName, items));
